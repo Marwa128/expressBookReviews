b258b53b825f5c47f355635f0f4bb0070e6cd207
Marwa128 <kh.m.off982@gmail.com>
2026-09-15 15:12:31 -0400

    Complete Task 11 with Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -49,25 +49,25 @@ public_users.get("/api/books", function (req, res) {
   });
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", async function (req, res) {
-  const isbn = req.params.isbn;
-
-  try {
-    const response = await axios.get("http://localhost:5000/");
-    const book = response.data[isbn];
-
-    if (!book) {
-      return res.status(404).json({
-        message: "Book not found"
+    const isbn = req.params.isbn;
+  
+    try {
+      const response = await axios.get("http://localhost:5000/");
+      const book = response.data[isbn];
+  
+      if (!book) {
+        return res.status(404).json({
+          message: "Book not found"
+        });
+      }
+  
+      res.send(JSON.stringify(book, null, 4));
+    } catch (error) {
+      res.status(500).json({
+        message: "Error retrieving book details"
       });
     }
-
-    res.send(JSON.stringify(book, null, 4));
-  } catch (error) {
-    res.status(500).json({
-      message: "Error retrieving book details"
-    });
-  }
-});
+  });
   
 // Get book details based on author
 public_users.get("/author/:author", async function (req, res) {
